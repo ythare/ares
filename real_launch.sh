3e7161bac9b7313429cfc779f2a8bc944068b668
ythare <2422807388@qq.com>
2024-12-01 21:10:21 +0800

    add sh

---
+++ real_launch.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+cmds=(
+    "
+    ros2 launch livox_ros_driver2 msg_MID360_launch.py 
+    "
+	"
+    ros2 launch rm_nav_bringup bringup_real.launch.py \
+    world:=RMUL \
+    mode:=nav \
+    lio:=fastlio \
+    localization:=slam_toolbox \
+    lio_rviz:=False \
+    nav_rviz:=True
+	"
+    "
+    ros2 launch rm_serial_driver rm_serial.launch.py 
+    "
+)
+
+for cmd in "${cmds[@]}"
+do
+	echo Current CMD : "$cmd"
+	gnome-terminal -- bash -c "cd $(pwd);source install/setup.bash;$cmd;exec bash;"
+	sleep 0.2
+done
